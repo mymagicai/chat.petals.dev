a284ff34199003a91af22d1d79b0c4845ae16151
vitali87 <eheva87@gmail.com>
2024-11-06 00:01:01 +0100

    chore: no idea what this last commit was meant for - just committing to delete the server

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import torch
-from petals.constants import PUBLIC_INITIAL_PEERS
+#from petals.constants import PUBLIC_INITIAL_PEERS
 
 from data_structures import ModelBackendConfig, ModelChatConfig, ModelConfig, ModelFrontendConfig
 
@@ -14,15 +14,6 @@ default_chat_config = ModelChatConfig(
 MODEL_FAMILIES = {
     "Llama 2": [
         ModelConfig(
-            ModelBackendConfig(repository="petals-team/StableBeluga2", aliases=["stabilityai/StableBeluga2"]),
-            ModelFrontendConfig(
-                name="Stable Beluga 2 (70B)",
-                model_card="https://huggingface.co/stabilityai/StableBeluga2",
-                license="https://huggingface.co/stabilityai/StableBeluga2/blob/main/LICENSE.txt",
-            ),
-            default_chat_config,
-        ),
-        ModelConfig(
             ModelBackendConfig(repository="meta-llama/Llama-2-70b-chat-hf"),
             ModelFrontendConfig(
                 name="Llama 2 (70B-Chat)",
@@ -32,65 +23,17 @@ MODEL_FAMILIES = {
             default_chat_config,
         ),
     ],
-    "Falcon": [
-        ModelConfig(
-            ModelBackendConfig(repository="tiiuae/falcon-180B-chat", public_api=False),
-            ModelFrontendConfig(
-                name="Falcon 180B-Chat",
-                model_card="https://huggingface.co/tiiuae/falcon-180B-chat",
-                license="https://huggingface.co/spaces/tiiuae/falcon-180b-license/blob/main/LICENSE.txt",
-            ),
-            ModelChatConfig(
-                max_session_length=8192,
-                sep_token="\n",
-                stop_token="\n",
-                extra_stop_sequences=["<|endoftext|>", "\nFalcon:", " Falcon:", "\nUser:", " User:", "###"],
-                generation_params=dict(do_sample=1, temperature=0.75, top_p=0.9, repetition_penalty=1.2),
-            ),
-        ),
-    ],
-    "Llama": [
-        ModelConfig(
-            ModelBackendConfig(repository="huggyllama/llama-65b", adapter="timdettmers/guanaco-65b"),
-            ModelFrontendConfig(
-                name="Guanaco-65B",
-                model_card="https://huggingface.co/timdettmers/guanaco-65b",
-                license="https://huggingface.co/timdettmers/guanaco-65b",
-            ),
-            default_chat_config,
-        ),
-        ModelConfig(
-            ModelBackendConfig(repository="huggyllama/llama-65b"),
-            ModelFrontendConfig(
-                name="Llama-65B",
-                model_card="https://github.com/facebookresearch/llama/blob/llama_v1/MODEL_CARD.md",
-                license="https://bit.ly/llama-license",
-            ),
-            default_chat_config,
-        ),
-    ],
-    "BLOOM": [
-        ModelConfig(
-            ModelBackendConfig(repository="bigscience/bloomz"),
-            ModelFrontendConfig(
-                name="BLOOMZ-176B",
-                model_card="https://huggingface.co/bigscience/bloomz",
-                license="https://bit.ly/bloom-license",
-            ),
-            ModelChatConfig(
-                max_session_length=2048,
-                sep_token="\n\n",
-                stop_token="</s>",
-                extra_stop_sequences=["\n\nHuman"],
-                generation_params=default_chat_config.generation_params,
-            ),
-        ),
-    ],
 }
 
-INITIAL_PEERS = PUBLIC_INITIAL_PEERS
+#INITIAL_PEERS = PUBLIC_INITIAL_PEERS
 # Set this to a list of multiaddrs to connect to a private swarm instead of the public one, for example:
-# INITIAL_PEERS = ['/ip4/10.1.2.3/tcp/31234/p2p/QmcXhze98AcgGQDDYna23s4Jho96n8wkwLJv78vxtFNq44']
+INITIAL_PEERS = [
+    "/ip4/86.48.21.229/tcp/31337/p2p/12D3KooWPyRUNtfz4f9RiPwhT99GVLvfVAALMXZ27AV1DubSc6oR",
+    # '/ip4/86.48.21.229/udp/31337/quic/p2p/12D3KooWJm4rZZDqwKU66wN7fzHk1aDbvkRjiDdxusQEPcHoH3X4',
+    "/ip4/81.0.246.28/tcp/31337/p2p/12D3KooWA6QWj4eP5D9xfsuAfarhDJX1nGhHasmFYJEVkjhSydyV",
+    # '/ip4/81.0.246.28/udp/31337/quic/p2p/12D3KooWFRqPYQ4o46F5dRQMshT24ypEzs8Qz5RMGHbHqHLT3J67'
+]
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
